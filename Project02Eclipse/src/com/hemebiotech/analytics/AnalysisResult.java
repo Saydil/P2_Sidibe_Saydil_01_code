894f871d61bfc889b9285682dff80560c21ac46d
Saydil Sidibe <saydilsidibe352@gmail.com>
2021-12-11 23:02:09 +0000

    Replace AnalyticsCounter by a new version named AnalysisResult

---
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalysisResult.java
@@ -0,0 +1,32 @@
+package com.hemebiotech.analytics;
+
+import java.util.TreeMap;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Set;
+
+/*
+ * Return a text file named "result.txt" with a list of symptoms and their occurrence
+ */
+
+public class AnalysisResult {
+	
+	public static void main(String args[]) throws IOException  {
+		
+		TreeMap<String, Integer> a = new TreeMap<String, Integer>(); 
+		AnalyzeSymptoms test = new AnalyzeSymptoms("C://Users/home/Desktop/Cours_Openclassrooms/Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application/Project02Eclipse/symptoms.txt");
+		
+		a.putAll(test.symptomsList());
+		
+		Set<String> keys = a.keySet();
+		FileWriter writer = new FileWriter (new File ("result.out"));
+		
+		
+        for(String key: keys){
+            writer.write(key + " : " + a.get(key) + "\n");
+        }
+		writer.close();
+	}
+
+}

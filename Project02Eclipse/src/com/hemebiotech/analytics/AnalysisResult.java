b1df18e859723f78e86ae61f58a5994e48269d62
Saydil Sidibe <saydilsidibe352@gmail.com>
2021-12-24 12:18:04 +0000

    Simplify AnalysisResult

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalysisResult.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalysisResult.java
@@ -1,32 +1,21 @@
 package com.hemebiotech.analytics;
 
-import java.util.TreeMap;
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Set;
 
 /*
- * Return a text file named "result.out" with a list of symptoms and their occurrence
+ * Return a text file named "result.txt" with a list of symptoms and their occurrence
  */
 
 public class AnalysisResult {
 	
-	public static void main(String args[]) throws IOException  {
-		
-		TreeMap<String, Integer> a = new TreeMap<String, Integer>(); 
-		AnalyzeSymptoms test = new AnalyzeSymptoms("symptoms.txt");
-		
-		a.putAll(test.symptomsList());
-		
-		Set<String> keys = a.keySet();
-		FileWriter writer = new FileWriter (new File ("result.out"));
+	public static void main(String args[]) throws IOException {
 		
 		
-        for(String key: keys){
-            writer.write(key + " : " + a.get(key) + "\n");
-        }
-		writer.close();
+		AnalyticsCounter test = new AnalyticsCounter("symptoms.txt");
+		test.symptomsList();
+		test.writeResult();
+		
+		
 	}
 
 }
